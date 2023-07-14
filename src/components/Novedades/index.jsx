7b3ae775891e475c747e94e08df72530ee01753d
Yami <yamilaveralopez@gmail.com>
2023-07-14 13:15:17 -0300

    feat: Add news feature

---
+++ src/components/Novedades/index.jsx
@@ -0,0 +1,14 @@
+import "./_news.scss"
+export const Novedades = () => {
+  return (
+    <div className="news_container">
+      <h2>Conoce nuestras Novedades</h2>
+      <section className="instagram_container">
+        <div
+          loading="lazy"
+          data-mc-src="8fd2d3ed-d2c1-4ccb-8f27-ffe8c9e79e98#instagram"
+        ></div>
+      </section>
+    </div>
+  );
+};
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,13 @@
+import { Novedades } from "../../components/Novedades";
 
 export default function Home() {
   return (
-    <div>
-        
+    <div>        
         <h1>Home</h1>
+        {/* Presentación */}
+        {/* Quienes somos */}
         
+        <Novedades/>
     </div>
   )
 }
