e172c43386166693b18fbae46128809690313a5f
abrilbril <abrilbrilnu@gmail.com>
2023-07-18 11:42:53 -0300

    change about us

---
+++ src/components/QuienesSomos/index.jsx
@@ -0,0 +1,26 @@
+import "./_news.scss";
+
+export const QuienesSomos = () => {
+    return (
+        <div className="quienes-somos-container">     
+            <h2 className="title">QUIENES SOMOS</h2>
+            <div className="quienes-somos-text-imagen">
+                <p className="paragraph1">
+                    Somos una fundación comprometida con la inclusión sociolaboral de personas en situación de vulnerabilidad. Trabajamos para construir comunidades empoderadas, fomentando la participación activa y el crecimiento colectivo. Conectamos actores de la sociedad civil, conocimientos y recursos para generar un impacto positivo en las comunidades que acompañamos.
+                </p>
+                <p className="paragraph1">
+                    Nuestro enfoque se basa en fortalecer la autonomía de quienes han sido excluidos a través del trabajo, brindándoles oportunidades de desarrollo y promoviendo la igualdad de oportunidades. Además, nos esforzamos por construir un futuro sostenible, creando procesos y productos.  
+                </p>
+            </div>  
+            <div>
+                <span><h3 className="subtitle">¿Cómo lo hacemos?</h3></span>
+                <span><h3 className="subtitle">Tenemos dos áreas de trabajo:</h3></span>
+            </div>
+            <div>
+
+            </div>
+                
+        </div>
+    );
+    
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,11 @@
+import { QuienesSomos } from "../../components/QuienesSomos";
 
 export default function Home() {
   return (
     <div>
         
         <h1>Home</h1>
+        <QuienesSomos/>
         
     </div>
   )
