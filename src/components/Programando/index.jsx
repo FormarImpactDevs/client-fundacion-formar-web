08a51ffadf61fb51c0bd5d6840bb4e0bc7a5af80
abrilbril <abrilbrilnu@gmail.com>
2023-07-24 18:15:42 -0300

    add color white in span

--- src/components/Programando/index.jsx
+++ src/components/Programando/index.jsx
@@ -4,15 +4,15 @@ import "./_programming.scss";
 export const Programando = () => {
     return (
         <div className="programando_container">
-                 <h1 className="title">&lt;Programando la inclusión/&gt;</h1>
+                 <h1 className="title"><span>&lt;</span>Programando la inclusión<span>/&gt;</span></h1>
            
             <section className="programando_text_img">
                 <div className="programando_text">
                     <span>
-                       <p className="paragraph">&gt;_ Es un puente entre las empresas que están liderando la revolución digital y un nuevo talento proveniente de comunidades vulneradas.</p> 
+                       <p className="paragraph"> <span>&gt;_</span> Es un puente entre las empresas que están liderando la revolución digital y un nuevo talento proveniente de comunidades vulneradas.</p> 
                     </span>
                     
-                    <p className="paragraph">&gt;_ Formamos y acompañamos en la inserción al mercado IT a las nuevas generaciones de programadores.</p>
+                    <p className="paragraph"> <span>&gt;_</span> Formamos y acompañamos en la inserción al mercado IT a las nuevas generaciones de programadores.</p>
                 </div>
                 <div className="programando_img">
                     <img src={programming} alt="programando la inclusión" />
