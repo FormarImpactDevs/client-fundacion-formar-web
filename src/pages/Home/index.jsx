19e4014567fa6a91b98da32c98933e2ad899a137
abrilbril <abrilbrilnu@gmail.com>
2023-07-27 14:25:21 -0300

    add component axes

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,6 +5,7 @@ import { QuienesSomos } from "../../components/QuienesSomos";
 import { Programando } from "../../components/Programando";
 import { Emprendiendo } from "../../components/Emprendiendo";
 import { Equipo } from "../../components/Team";
+import { Ejes } from "../../components/Axes";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <MainLayout>
         <Presentation />
         <QuienesSomos />
+        <Ejes/>
         <Equipo/>
         <Programando />
         <Emprendiendo/>
