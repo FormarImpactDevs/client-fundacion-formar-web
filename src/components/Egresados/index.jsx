10784c15fd557ffedb43d842fad29518e490cf27
Jonatan Céspedes <66534017+JonatanCespedes@users.noreply.github.com>
2023-07-29 09:32:28 -0300

    Revert "Feature/graduates"

--- src/components/Egresados/index.jsx
+++ src/components/Egresados/index.jsx
@@ -1,20 +1,19 @@
-import video from "../../assets/video.mp4"
+import ReactPlayer from "react-player";
 import "./_egresados.scss";
 
 
 export const Egresados =() => {
     return (
-        <div className="egresados_container">
-            <h1 className="title">Conoce a los egresados</h1>
-
-            <section className="egresados_text_video">
+        <section className="egresados_container">
+            <h1 className="title">Conociendo a los egresados</h1>
             <div className="egresados_text">
-                <h2>Te compartimos los testimonios de algunos egresados de Programando la Inclusión</h2>        
-                </div>
-                <div className="video">
-                <video width="100%" height="300px"src ={video} controls/>
-                </div> 
-           </section>
-         </div>
+                <p>Conoce los testimonios de algunos de <br/>
+                los egresados de Programando la Inclusión.</p>        
+                <ReactPlayer url="https://drive.google.com/file/d/142IkCCbOZsK1pC7HBOtvFNGRigfR4rjJ/view?usp=sharing" 
+           controls
+           className= "Player"/>
+           </div>
+           
+       </section>
     )
 } 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,10 +4,6 @@ import { MainLayout } from "../../layout";
 import { QuienesSomos } from "../../components/QuienesSomos";
 import { Programando } from "../../components/Programando";
 import { Egresados } from "../../components/Egresados";
-import { Emprendiendo } from "../../components/Emprendiendo";
-import { Equipo } from "../../components/Team";
-import { Ejes } from "../../components/Axes";
-import { EquipoEmprendiendo } from "../../components/TeamUndertaking";
 
 export default function Home() {
   return (
@@ -15,12 +11,8 @@ export default function Home() {
       <MainLayout>
         <Presentation />
         <QuienesSomos />
-        <Ejes/>
-        <Equipo/>
         <Programando />
         <Egresados />
-        <Emprendiendo/>
-        <EquipoEmprendiendo/>
         <Novedades />
       </MainLayout>
     </>
