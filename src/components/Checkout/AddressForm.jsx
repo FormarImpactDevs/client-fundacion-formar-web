fdf2d4219e7be2ec6db9cfe94cdad6aff06c3b53
Aixa Flores <aixafloressantillan@gmail.com>
2023-10-02 14:54:25 -0300

    fix: add product payment modifications

--- src/components/Checkout/AddressForm.jsx
+++ src/components/Checkout/AddressForm.jsx
@@ -1,17 +1,47 @@
 import React from 'react';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import "./checkout.scss";
+import { useState } from "react";
+import {
+  Radio,
+  FormGroup,
+  FormControlLabel, 
+  FormControl, 
+  RadioGroup,
+  TextField,
+  Typography, 
+  Grid, 
+  Checkbox
+}from '@mui/material';
 
+const CssTextField = styled(TextField)({
+  "& label.Mui-focused": {
+    color: "#75aadb",
+  },
+  "& .MuiInput-underline:after": {
+    borderBottomColor: "#75aadb",
+  },
+  "& .MuiOutlinedInput-root": {
+    "& fieldset": {
+      borderColor: "#E0E3E7",
+    },
+    "&:hover fieldset": {
+      borderColor: "#75aadb",
+    },
+    "&.Mui-focused fieldset": {
+      borderColor: "#75aadb",
+    },
+  },
+});
 
 export default function AddressForm() {
+  const [radio, setRadio] = useState("");
+
+  const handleRadioChange = (event) => {
+    setRadio(event.target.value);
+  };
   return (
     <React.Fragment>
-      <Typography variant="h6" gutterBottom className='typography'>
-       Direccion de envio
+      <Typography variant="h3" gutterBottom className='typography'>
+      Ingresá tus datos
       </Typography>
       <Grid container spacing={3}>
         <Grid item xs={12} sm={6}>
@@ -59,16 +89,6 @@ export default function AddressForm() {
             variant="standard"
           />
         </Grid>
-        <Grid item xs={12}>
-          <TextField
-            id="address2"
-            name="address2"
-            label="Direccion y numero"
-            fullWidth
-            autoComplete="shipping address-line"
-            variant="standard"
-          />
-        </Grid>
         <Grid
                 item
                 xs={12}
@@ -76,42 +96,109 @@ export default function AddressForm() {
                 justifyContent="flex-center"
                 sx={{ m: 0 }}
               >  <Typography variant="h3">
-              Puntos de retiro
+              Como lo recibo o retiro
              </Typography>
              <Typography variant="h5" gutterBottom>
-             Elegí el punto de retiro gratuito que te convenga y te contactaremos para coordinar la entrega.
+             Elegí el punto de retiro gratuito que te convenga y te 
+             contactaremos para coordinar la entrega. 
              </Typography>
              </Grid>
+             <Grid>
+                <FormControl>
+                  <RadioGroup
+                    row
+                    aria-labelledby="demo-row-radio-buttons-group-label"
+                    name="row-radio-buttons-group"
+                    onChange={handleRadioChange}
+                  >
+                    <Grid
+                      container
+                      direction="row"
+                      justifyContent="center"
+                      alignItems="center"
+                      sx={{ ml: 3 }}
+                    >
+                      <Grid item xs={12} sm={6}>
+                        <FormControlLabel
+                          value="punto de retiro"
+                          control={<Radio size="small" />}
+                          label="Punto de retiro"
+                        />
+                      </Grid>
+                      <Grid item xs={12} sm={6}>
+                        <FormControlLabel
+                          value="donante"
+                          control={<Radio size="small" />}
+                          label="Envio"
+                        />
+                      </Grid>
+                    </Grid>
+                  </RadioGroup>
+                </FormControl>
+              </Grid>
+              {radio && radio === "voluntario" ? (
+                <div className="checkboxes">
+                  <Box sx={{ display: "flex" }}>
+                    <FormControl
+                      sx={{ m: 3 }}
+                      row
+                      component="fieldset"
+                      variant="standard"
+                    >
+                      <Grid
+                        container
+                        direction="row"
+                        justifyContent="space-around"
+                        alignItems="center"
+                      >
+                        <Grid item xs={12} sm={6}>
+                          <FormGroup>
+                            <FormControlLabel
+                              control={<Checkbox name="tutoriasDeIngles" />}
+                              label="Tutorías de inglés"
+                            />
+                            <FormControlLabel
+                              control={
+                                <Checkbox name="mentoriasEnHabilidadesTecnicas" />
+                              }
+                              label="Mentorías en habilidades técnicas"
+                            />
+                            <FormControlLabel
+                              control={
+                                <Checkbox name="mentoriasEnHabilidadesBlandas" />
+                              }
+                              label="Mentorías en habilidades blandas"
+                            />
+                            <FormControlLabel
+                              control={<Checkbox name="dictarUnaMasterclass" />}
+                              label="Dictar una masterclass"
+                            />
+                          </FormGroup>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                          {" "}
+                          <FormGroup>
+                            <FormControlLabel
+                              control={<Checkbox name="otro" />}
+                              label="Otro"
+                            />
 
-        <Grid container spacing={2} className='Retiros'>
-             <Grid item xs={12} sm={6} className='puntoRetiro'>
-            <FormControlLabel
-              control={<Checkbox color="secondary" name="saveAddress" value="yes" />}
-               label="Floresta (CABA)"
-             />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-             <FormControlLabel
-              control={<Checkbox color="secondary" name="saveAddress" value="yes" />}
-              label="Polo de Palermo (CABA)"
-           />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-             <FormControlLabel
-              control={<Checkbox color="secondary" name="saveAddress" value="yes" />}
-              label="Boulogne"
-           />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-             <FormControlLabel
-              control={<Checkbox color="secondary" name="saveAddress" value="yes" />}
-              label="Benavidez"
-           />
-        </Grid>
-        
-   </Grid>
+                            <CssTextField
+                              id="outlined-multiline-static"
+                              multiline
+                              rows={4}
+                              defaultValue=""
+                            />
+                          </FormGroup>
+                        </Grid>
+                      </Grid>
+                    </FormControl>
+                  </Box>
+                </div>
+              ) : (
+                ""
+              )}
  </Grid>
-     
-    </React.Fragment>
+</React.Fragment>
   );
 }
