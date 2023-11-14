d6ea0b5dd74593aec65a0b9c819fb42ce7c353ad
AbrilAgus <abrilbrilnu@gmail.com>
2023-11-14 11:21:38 -0300

    view product cart fin

--- src/pages/HomeEcommerce/index.jsx
+++ src/pages/HomeEcommerce/index.jsx
@@ -1,6 +1,6 @@
 import { MainLayout } from "../../layout";
 import ProductsEcommerce from '../../components/ProductsEcommerce';
-import { ProductDetail } from '../../components/ProductDetail';
+import { ProductDetail } from '../ProductDetail';
 import { ProductCart } from "../../components/ProductCart";
 import Checkout from "../../components/Checkout/checkout";
 import { LayoutProducts } from "../../layout/LayoutProducts";
