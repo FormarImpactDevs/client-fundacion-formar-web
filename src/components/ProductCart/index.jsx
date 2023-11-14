7a27ec5f34aa160ca85aa1a2817860a767c1b020
AbrilAgus <abrilbrilnu@gmail.com>
2023-11-14 11:52:10 -0300

    fin detail

--- src/components/ProductCart/index.jsx
+++ src/components/ProductCart/index.jsx
@@ -12,29 +12,28 @@ export const ProductCart = () => {
           <div className="productCart-product-image">
             <img src={image1} alt="" />
           </div>
-          <div className="productCart-product-info">
+          <div className="productCart-info-product">
             <h2 className="paragraph2">Nombre del producto</h2>
             <h2 className="paragraph2">Precio</h2>
             <form action="">
               <p className="paragraph2">Cantidad</p>
               <input type="number" name="quantity" id="Cantidad"></input>
             </form>
+            <a href="">
+              <Button
+                variant="contained"
+                size="large"
+                className="button"
+                sx={{
+                  color: "secondary.light",
+                }}
+              >
+                Eliminar
+              </Button>
+            </a>
           </div>
         </div>
-        <div className="productCart-producto">
-          <div className="productCart-product-image">
-            <img src={image1} alt="" />
-          </div>
-          <div className="productCart-product-info">
-            <h2 className="paragraph2">Nombre del producto</h2>
-            <h2 className="paragraph2">Precio</h2>
-            <form action="">
-              <p className="paragraph2">Cantidad</p>
-              <input type="number" name="quantity" id="Cantidad"></input>
-            </form>
-          </div>
-        </div>
-        
+
         <div className="productCart-info">
           <h2 className="paragraph2">Subtotal: $9999</h2>
           <a href="">
@@ -49,6 +48,14 @@ export const ProductCart = () => {
               Iniciar compra
             </Button>
           </a>
+          <h2 className="paragraph2">Envío:</h2>
+          <form action="" method="get">
+            <label></label>
+            <select name="envio" id="envio">
+              <option value="">Punto de retiro gratuito</option>
+              <option value="">Cotizá tu envío</option>
+            </select>
+          </form>
         </div>
       </div>
     </div>
