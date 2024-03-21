3e8a461f18d315dea77858dde01b1a91d4fe220d
MelinaLabra <melina.lb04@gmail.com>
2024-03-21 19:10:12 -0300

    add alert

--- src/views/Feedback/index.jsx
+++ src/views/Feedback/index.jsx
@@ -12,8 +12,23 @@ export const Feedback = () => {
   </div>
   <div style={{display: "flex", position: "relative",}}>
     <Alert severity="success">El pago ha sido realizado correctamente</Alert>
+    <Alert severity="warning">El pago esta pendiente</Alert>
+    <Alert severity="error">El pago fue fallido</Alert>
   </div>
 </div>
     </MainLayout>
   );
 };
+//aca te proporciono las rutas >>>>>>>>>>>
+/*
+back_urls: {
+failure:   isChange 
+?  process.env.BASE_URL + "/feedback/failure"
+:   process.env.BASE_URL + "/feedback/failure"
+pending:  idChange
+?  process.env.BASE_URL + "/feedback/pending"
+:   process.env.BASE_URL + "/feedback/pending"
+success:  isChange 
+?  process.env.BASE_URL + "/feedback/success"
+:   process.env.BASE_URL + "/feedback/success"
+*/
