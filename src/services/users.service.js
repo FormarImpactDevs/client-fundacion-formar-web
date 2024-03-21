cfba3f35aa9eb31edd196ac1641c575b2e68883e
Yami <yamilaveralopez@gmail.com>
2024-03-21 13:06:16 -0300

    arrelo detalle de comillas

--- src/services/users.service.js
+++ src/services/users.service.js
@@ -2,7 +2,7 @@ const BASE_USERS_URL_API = "http://localhost:3000/api/users";
 
 export async function userLoginService(userData) {
   try {
-    const result = await fetch(`${BASE_USERS_URL_API}`/login, {
+    const result = await fetch(`${BASE_USERS_URL_API}/login`, {
       method: "POST",
       headers: {
         'Content-Type': 'application/json',
