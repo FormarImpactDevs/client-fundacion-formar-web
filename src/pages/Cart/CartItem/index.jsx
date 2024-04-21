6baaeb9eda74abf0c12d5156baacdba8eaf94375
Yami <yamilaveralopez@gmail.com>
2024-04-21 19:33:41 -0300

    Añado estilos al carrito

--- src/pages/Cart/CartItem/index.jsx
+++ src/pages/Cart/CartItem/index.jsx
@@ -15,43 +15,98 @@ const CartItem = ({ item, handleRemove, handleQuantityChange }) => {
   const total = quantity * precio;
 
   return (
-    <TableRow>
-      <TableCell>
-        <CardMedia
-          component="img"
-          alt={nombre}
-          image={images && images.length > 0 ? images[0].imagen : ""}
-          style={{ width: 50, height: 50 }}
-        />
-      </TableCell>
-      <TableCell>{nombre}</TableCell>
-      <TableCell>${precio}</TableCell>
-      <TableCell>
-        <Box display="flex" alignItems="center">
-          <IconButton
-            onClick={() =>
-              handleQuantityChange(item.id, quantity - 1)
-            } /*  disabled={cantidad <= 1} */
-          >
-            -
+    <>
+      {/* Para dispositivos móviles y tablets */}
+      <TableRow
+        className="paragraph2"
+        sx={{
+          color: "#58595b",
+          textAlign: "center",
+          display: { xs: "flex", sm: "none" },
+          justifyContent: "center",
+        }}
+      >
+        <TableCell>
+          <CardMedia
+            component="img"
+            alt={nombre}
+            image={images && images.length > 0 ? images[0].imagen : ""}
+            style={{ width: 60, height: 60 }}
+          />
+        </TableCell>
+        <TableCell>
+          <Typography variant="body1">
+            <strong>{nombre}</strong>
+          </Typography>
+          <Typography variant="body2">${total}</Typography>
+        </TableCell>
+        <TableCell>
+          <Box display="flex" alignItems="center">
+            <IconButton
+              onClick={() => handleQuantityChange(item.id, quantity - 1)}
+            >
+              -
+            </IconButton>
+            <Typography variant="body1">{quantity}</Typography>
+            <IconButton
+              onClick={() => handleQuantityChange(item.id, quantity + 1)}
+            >
+              +
+            </IconButton>
+          </Box>
+        </TableCell>
+        <TableCell>
+          <IconButton onClick={() => handleRemove(item.id)} color="secondary">
+            <FontAwesomeIcon icon={faTrash} />
           </IconButton>
-          <Typography variant="body1">{quantity}</Typography>
-          <IconButton
-            onClick={() =>
-              handleQuantityChange(item.id, quantity + 1)
-            } /* disabled={cantidad >= item.stock} */
-          >
-            +
+        </TableCell>
+      </TableRow>
+
+      {/* Para dispositivos de escritorio */}
+      <TableRow
+        className="paragraph2"
+        sx={{
+          color: "#58595b",
+          textAlign: "center",
+          display: { xs: "none", sm: "table-row" },
+          justifyContent: "space-around",
+          alignItems: "center",
+          borderBottom: "1px solid #e0e0e3",
+        }}
+      >
+        <TableCell>
+          <CardMedia
+            component="img"
+            alt={nombre}
+            image={images && images.length > 0 ? images[0].imagen : ""}
+            style={{ width: 75, height: 75 }}
+          />
+        </TableCell>
+        <TableCell>{nombre}</TableCell>
+        <TableCell>${precio}</TableCell>
+        <TableCell>
+          <Box display="flex" alignItems="center">
+            <IconButton
+              onClick={() => handleQuantityChange(item.id, quantity - 1)}
+            >
+              -
+            </IconButton>
+            <Typography variant="body1">{quantity}</Typography>
+            <IconButton
+              onClick={() => handleQuantityChange(item.id, quantity + 1)}
+            >
+              +
+            </IconButton>
+          </Box>
+        </TableCell>
+        <TableCell>${total}</TableCell>
+        <TableCell>
+          <IconButton onClick={() => handleRemove(item.id)} color="secondary">
+            <FontAwesomeIcon icon={faTrash} />
           </IconButton>
-        </Box>
-      </TableCell>
-      <TableCell>${total}</TableCell>
-      <TableCell>
-        <IconButton onClick={() => handleRemove(item.id)} color="secondary">
-          <FontAwesomeIcon icon={faTrash} />
-        </IconButton>
-      </TableCell>
-    </TableRow>
+        </TableCell>
+      </TableRow>
+    </>
   );
 };
 
