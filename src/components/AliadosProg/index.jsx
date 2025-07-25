0cbdcb244eaf868a9ccb425a468cac3cd0adbbb5
Tabitoo <lautarovaldez4@gmail.com>
2025-07-25 20:27:43 -0300

    feat: change carousel styles

--- src/components/AliadosProg/index.jsx
+++ src/components/AliadosProg/index.jsx
@@ -1,11 +1,9 @@
+import accenture from "../../assets/accenture.png"
 import dh from "../../assets/dh.png";
 import santander from "../../assets/santander.png";
 import sanMiguel from "../../assets/sanmiguel.png";
-import lanus from "../../assets/municipio_lanus.png";
+import empower from "../../assets/empower.png";
 import pampaEnergia from "../../assets/pampaenergia.png"
-import accenture from "../../assets/accenture.png"
-import pedidos from "../../assets/pedidosya.png"
-import arredos from "../../assets/arredo.png"
 import Slide3d from "../Slider3d";
 import "./_aliadosP.scss";
 import Title from "../Title"
@@ -27,10 +25,7 @@ export const AliadosProg =() => {
       img: sanMiguel,
     
     },
-    {
-      img: lanus,
     
-    },
     {
       img: pampaEnergia,
      
@@ -40,11 +35,7 @@ export const AliadosProg =() => {
       
     },
     {
-      img: pedidos,
-      
-    },
-    {
-      img: arredos,
+      img: empower,
       
     },
   
@@ -54,20 +45,9 @@ export const AliadosProg =() => {
         <section className="aliadosp_container">
             <Title text="Hacen posible este programa  "/>
             <Subtitle text="Empresas y organizaciones comprometidas con la inclusión laboral en el sector IT" />
-            <div>        
+            <div className="row">        
                     <Slide3d list={cardsInfo}  />
                   </div>
-           {/* <div className="row">
-            <div className="img_aliados"><img src={santander} alt="Logo santander"/></div>
-            <div className="img_aliados"><img src={dh} alt="Logo Digital House"/></div>
-            <div className="img_aliados"><img src={sanMiguel} alt="Logo Municipalidad San Miguel"/></div>
-            <div className="img_aliados"><img src={ pampaEnergia } alt="Logo Pampa energia " /></div>
-            <div className="img_aliados"><img className="lanus" src={lanus} alt="Logo Municipalidad de Lanus" /></div>
-            <div className="img_aliados"><img className="accenture" src={accenture} alt="Logo Accenture" /></div>
-            <div className="img_aliados"><img className="lanus" src={pedidos} alt="Logo pedidos ya" /></div>
-            <div className="img_aliados"><img className="lanus" src={arredos} alt="Logo Arredos" /></div>
-           </div> */}
-           
        </section>
     )
 } 
